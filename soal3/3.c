75c6119c55c83daf8a5f889021e1e178c4122f51
rafif <rafifridhoo@gmail.com>
2020-04-03 17:39:01 +0700

    add no3

---
+++ soal3/3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h> 
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<dirent.h>
+
+int main(){
+	return 0;
+}
